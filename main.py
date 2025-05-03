ecd8e64c021cc21aa2691bb11c3deb581a0dd16d
Evan Handgraaf <eje.handgraaf@gmail.com>
2025-05-04 01:27:06 +0200

    CRUD

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@app.route("/", methods=["GET"])
+def home():
+    return "Hello, World!", 200
+
+@app.route("/create", methods=["POST"])
+def create():
+    return "Create operation", 200
+
+@app.route("/read", methods=["GET"])
+def read():
+    return "Read operation", 200
+
+@app.route("/update", methods=["PUT"])
+def update():
+    return "Update operation", 200
+
+@app.route("/delete", methods=["DELETE"])
+def delete():
+    return "Delete operation", 200
