f926ad1f60b6ca9b3704fb54ca3939ba2dd6a228
Evan Handgraaf <eje.handgraaf@gmail.com>
2025-05-05 11:46:09 +0200

    Update version

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def version():
       200:
         description: Application version in JSON format
     """
-    return {"version": "0.0.1"}
+    return {"version": "0.0.2"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
